474d964aa9ff6bd4f068ec12b3ab0ad72e95fd96
epiph <epiphanymuriuki7@gmail.com>
2019-08-08 14:17:56 +0300

    Added Authentication and Storage

--- app/src/main/java/com/example/travelmantics/DealActivity.java
+++ app/src/main/java/com/example/travelmantics/DealActivity.java
@@ -1,39 +1,67 @@
 package com.example.travelmantics;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Switch;
 import android.widget.Toast;
 
-import com.google.firebase.database.DatabaseException;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class InsertActivity extends AppCompatActivity {
+public class DealActivity extends AppCompatActivity {
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mDatabaseReference;
+    private static final int PICTURE_RESULT =42;
     EditText txtTitle;
     EditText txtDescription;
     EditText txtPrice;
+    TravelDeal deal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        FirebaseUtil.openFbReference("traveldeals");
+        setContentView(R.layout.activity_deal);
+        /*FirebaseUtil.openFbReference("traveldeals",this);*/
         mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
         mDatabaseReference =FirebaseUtil.mDatabaseReference;
 
         txtTitle = findViewById(R.id.txtTitle);
         txtDescription =findViewById(R.id.txtDescription);
         txtPrice =findViewById(R.id.txtPrice);
+
+        Intent intent =getIntent();
+        TravelDeal deal = (TravelDeal) intent.getSerializableExtra("Deal");
+        if(deal==null){
+            deal =new TravelDeal();
+
+        }
+        this.deal=deal;
+        txtTitle.setText(deal.getTitle());
+        txtDescription.setText(deal.getDescription());
+        txtPrice.setText(deal.getPrice());
+        Button btnImage=findViewById(R.id.btnImage);
+        btnImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/jpeg");
+                intent.putExtra(intent.EXTRA_LOCAL_ONLY,true);
+                startActivityForResult(intent.createChooser(intent,"Insert Picture"),42);
+
+            }
+        });
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -42,6 +70,12 @@ public class InsertActivity extends AppCompatActivity {
                 saveDeal();
                 Toast.makeText(this,"Deal Saved",Toast.LENGTH_LONG).show();
                 clean();
+                backToList();
+                return true;
+            case R.id.delete_menu:
+                deleteDeal();
+                Toast.makeText(this,"Deal Deleted Successfully",Toast.LENGTH_LONG).show();
+                backToList();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -56,13 +90,38 @@ public class InsertActivity extends AppCompatActivity {
         return true;
 
     }
-    private void saveDeal(){
-        String title = txtTitle.getText().toString();
-        String description =txtDescription.getText().toString();
-        String price = txtPrice.getText().toString();
-        TravelDeal deal = new TravelDeal(title,description,price,"");
-        mDatabaseReference.push().setValue(deal);
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==PICTURE_RESULT && resultCode ==RESULT_OK){
+
+        }
+    }
+
+    private void saveDeal(){
+        deal.setTitle( txtTitle.getText().toString());
+        deal.setTitle( txtDescription.getText().toString());
+        deal.setPrice( txtPrice.getText().toString());
+        if(deal.getId()==null){
+            mDatabaseReference.push().setValue(deal);
+        }
+     else {
+         mDatabaseReference.child(deal.getId()).setValue(deal);
+        }
+
+    }
+    private void deleteDeal(){
+        if (deal==null){
+            Toast.makeText(this,"Please Enter the deal Before Deleteing",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mDatabaseReference.child(deal.getId()).removeValue();
+    }
+
+    private void backToList(){
+        Intent intent =new Intent(this,ListActivity.class);
+        startActivity(intent);
     }
 
     private void clean(){
